573619b67713e61b11024df26f396539d53efca6
John Hawthorn <john.hawthorn@gmail.com>
2017-04-19 22:33:40 -0700

    Use nested, not rails-style, module declaration

--- lib/discard/model.rb
+++ lib/discard/model.rb
@@ -1,17 +1,19 @@
-module Discard::Model
-  extend ActiveSupport::Concern
+module Discard
+  module Model
+    extend ActiveSupport::Concern
 
-  included do
-    scope :kept, ->{ where(discarded_at: nil) }
-    scope :discarded, ->{ where.not(discarded_at: nil) }
-    scope :with_discarded, ->{ unscope(where: :discarded_at) }
-  end
+    included do
+      scope :kept, ->{ where(discarded_at: nil) }
+      scope :discarded, ->{ where.not(discarded_at: nil) }
+      scope :with_discarded, ->{ unscope(where: :discarded_at) }
+    end
 
-  def discarded?
-    !!discarded_at
-  end
+    def discarded?
+      !!discarded_at
+    end
 
-  def discard
-    touch(:discarded_at) unless discarded?
+    def discard
+      touch(:discarded_at) unless discarded?
+    end
   end
 end
